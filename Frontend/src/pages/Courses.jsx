afeaa431ed7aafc40dc32045924fcf1cf3d4bb9e
Aryan <arynch24@gmail.com>
2025-05-28 12:29:11 +0530

    Add Mentor component and integrate it into Courses page

--- Frontend/src/pages/Courses.jsx
+++ Frontend/src/pages/Courses.jsx
@@ -1,10 +1,12 @@
 import OurCourses from "../components/OurCourses";
 import CoursesSec from "../components/CoursesSec";
+import Mentors from "../components/Mentors";
 export default function Courses() {
     return (
         <>
             <OurCourses />
             <CoursesSec />
+            <Mentors />
         </>
     );
 }
